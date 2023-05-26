a8856bfa7ce84f628808ccdd484551838893c6dc
Denis Mironov <denis-mironov1199@yandex.ru>
2023-05-26 11:59:57 +0300

    flake #6

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from reviews import models
 
+
 admin.site.register(models.Category)
 admin.site.register(models.Genre)
 admin.site.register(models.Title)
--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -2,7 +2,6 @@ from django.core.exceptions import ValidationError
 from django.utils import timezone
 
 
-
 def year_validator(value):
     if value > timezone.now().year:
         raise ValidationError(
