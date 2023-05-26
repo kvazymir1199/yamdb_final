a0382c24e2205403678097e58a1d5dc3d04a40c0
Denis Mironov <denis-mironov1199@yandex.ru>
2023-05-26 12:18:04 +0300

    #1

--- api_yamdb/api/v1/serializers.py
+++ api_yamdb/api/v1/serializers.py
@@ -2,7 +2,6 @@ import re
 
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
-
 from reviews.models import Category, Comment, Genre, Review, Title
 from users.models import User
 
